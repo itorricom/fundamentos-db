b2db99275dffc39259aa24e1f2c101be72cf006d
Isidoro Torrico <itorrico.m@gmail.com>
2019-11-15 15:16:26 -0400

    archvios base de fundamentos de base de datos

---
+++ WHERE.sql
@@ -0,0 +1,24 @@
+SELECT *
+FROM posts
+WHERE id > 50;
+
+SELECT *
+FROM posts
+WHERE estatus = 'inactivo';
+
+SELECT *
+FROM posts
+WHERE estatus != 'activo';
+
+SELECT *
+FROM posts
+WHERE titulo LIKE '%escandalo%';
+
+SELECT *
+FROM posts
+WHERE titulo LIKE '%escandalo%';
+
+SELECT *
+FROM posts
+WHERE titulo LIKE '%escandalo%';
+
---
+++ introduccion_db-master/Select By ISI.sql
@@ -0,0 +1,9 @@
+SELECT titulo AS Encabezado, fecha_publicacion AS Publicado, estatus as Estado
+FROM posts;
+
+SELECT COUNT(*)
+FROM posts;
+
+SELECT COUNT(*) AS numero_posts
+FROM posts;
+
