e815ad9b6c62989a707ba96333175a5c7748727b
Isidoro Torrico <itorrico.m@gmail.com>
2019-11-15 16:43:13 -0400

    Where finalizado

--- WHERE.sql
+++ WHERE.sql
@@ -20,5 +20,34 @@ WHERE titulo LIKE '%escandalo%';
 
 SELECT *
 FROM posts
-WHERE titulo LIKE '%escandalo%';
+WHERE fecha_publicacion > '2025-01-01';
+
+SELECT *
+FROM posts
+WHERE fecha_publicacion BETWEEN '2023-01-01' AND '2025-12-31';
+
+SELECT *
+FROM posts
+WHERE YEAR(fecha_publicacion) BETWEEN '2023' AND '2024';
+
+SELECT *
+FROM posts
+WHERE MONTH(fecha_publicacion) ='04';
+
+---- Utilizando la sentencia WHERE nulo y no nulo -----
+
+SELECT *
+FROM posts
+WHERE usuario_id IS NULL;
+
+SELECT *
+FROM posts
+WHERE categoria_id IS NULL;
+
+SELECT *
+FROM posts
+WHERE categoria_id IS NOT NULL
+	AND estatus = 'activo';
+
+
 
